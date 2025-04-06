2bbd00bd37163e7c2d4dc3479152dcc74b5d4bed
Andrei Lavrenov <andrei@lavrenov.io>
2025-04-06 13:12:55 +0200

    cleanup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 ## 0.1.0 (2025-04-06)
 
--   initialize project with `npx create-next-app@latest`
--   add vitest for testing
--   add prisma for database
--   add posthog for analytics
+- initialize project with `npx create-next-app@latest`
+- add vitest for testing
+- add prisma for database
+- add posthog for analytics
+- add TailwindCSS & ShadCN for styling
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
     return (
         <main className="flex">
             <h1 className="text-3xl font-bold underline">tailwindcss</h1>main
             content here
+            <Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
         </main>
     );
 }
